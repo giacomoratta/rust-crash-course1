b4b44a558fa7f54eee6b78a9cab5acfc02b7893e
grmxd <giacomo.ratta@diymaxeda.com>
2020-08-13 06:54:15 +0200

    first

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod arrays;
+// mod tuples;
+// mod strings;
+// mod types;
+// mod vars;
+// mod print;
+
+fn main() {
+    // print::run();
+    // vars::run();
+    // types::run();
+    // strings::run();
+    // tuples::run();
+    arrays::run();
+}
