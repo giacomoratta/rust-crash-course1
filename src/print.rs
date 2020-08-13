7bc706ada97faa87d02b297209596d17f7e445c5
grmxd <giacomo.ratta@diymaxeda.com>
2020-08-13 07:08:10 +0200

    first

---
+++ src/print.rs
@@ -0,0 +1,16 @@
+pub fn run() {
+    println!("Hello from the print.rs file");
+    println!("{} is from {}", "Brad", "Mass");
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Brad", "Mass", "code"
+    );
+    println!(
+        "{name} likes to play {activity}",
+        name = "John",
+        activity = "Baseball"
+    );
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+    println!("{:?}", (12, true, "hello"));
+    println!("10 + 10 = {}", 10 + 10);
+}
