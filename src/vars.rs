d0ef986814994fb11b2ed0d807695bb515536e93
grmxd <giacomo.ratta@diymaxeda.com>
2020-08-13 06:56:59 +0200

    Revert "first"
    This reverts commit b4b44a558fa7f54eee6b78a9cab5acfc02b7893e.

--- src/vars.rs
+++
@@ -1,8 +0,0 @@
-pub fn run() {
-    println!("Hello from printer");
-    println!("{} is from {}", "Brad", "Mass");
-    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "code");
-    println!("{name} likes to play {activity}", name="John", activity="Baseball");
-    println!("Binary: {:b} Hex: {:x}, Octal: {:o}", 10, 10, 10);
-    println!("{:?}", (12, true, "hello"));
-}
